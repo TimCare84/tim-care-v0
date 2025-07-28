ac49b96bf05db2e67f2b8857c9ccdf65e9306bba
santidevhmo <santiagoagdc42@gmail.com>
2025-07-28 08:59:14 -0700

    TC-84 Generar interfaces y types basado en schema de DB

---
+++ schemas/customers.ts
@@ -0,0 +1,12 @@
+
+export type Customer = {
+    id: string
+    whatsapp_number?: string
+    user_name?: string
+    email?: string
+    last_interaction?: string
+    clinic_id: string
+    agent_active: boolean
+    created_at: string
+    updated_at: string
+}
